5ae108490a06db855091517c2e9bbe50b2c22c6a
feckneck <math.dousse@hotmail.fr>
2023-12-12 20:18:07 +0100

    feat: add router and env file

--- apps/front/src/router.ts
+++ apps/front/src/router.ts
@@ -1,10 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
-    path: "/",
-    name: "Menu",
-    component: () => import("./components/Menu.vue"),
+    path: '/',
+    name: 'Menu',
+    component: () => import('./components/Menu.vue'),
+  },
+  {
+    path: '/monsters',
+    name: 'Monsters',
+    component: () => import('./components/Monster/MonsterList.vue'),
   },
 ];
 
